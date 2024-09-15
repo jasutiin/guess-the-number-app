d3ce1e60d2d0a5c5cf2882b17d2a7b970d408a49
justine <justinemangaliman@gmail.com>
2024-09-14 23:50:49 -0600

    design done

---
+++ components/PrimaryButton.js
@@ -0,0 +1,47 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+
+function PrimaryButton(props) {
+  function pressHandler() {
+    console.log('pressed');
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+    flex: 1,
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
---
+++ screens/GameScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function GameScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>GameScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
