9a6553221cd0e213bafed5be251ff248e89af327
justine <justinemangaliman@gmail.com>
2024-09-17 18:25:49 -0600

    changed styling

--- constants/colors.js
+++ constants/colors.js
@@ -1,9 +1,9 @@
 const Colors = {
-  primary500: '#72063c',
-  primary600: '#640233',
-  primary700: '#4e0329',
-  primary800: '#3b021f',
-  accent500: '#ddb52f',
+  primary500: '#0264bf',
+  primary600: '#00509d',
+  primary700: '#003f88',
+  primary800: '#00296b',
+  accent500: '#a69cac',
 };
 
 export default Colors;
